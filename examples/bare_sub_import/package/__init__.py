ec4456bf81f4a29ac4e260ebc2d0f150764b617c
Dorian Turba <dturba.pro@protonmail.com>
2022-09-18 18:38:46 +0200

    [docs] correct bare_sub_import example

--- examples/bare_sub_import/package/__init__.py
+++ examples/bare_sub_import/package/__init__.py
@@ -1,6 +1,6 @@
 import lazimp
 
-sub_package: lazimp.ModuleType
+package_2: lazimp.ModuleType
 
 __getattr__ = lazimp.lazy_import('package_2.bare_sub_module',
                                  'package_2.bare_sub_module_2')
