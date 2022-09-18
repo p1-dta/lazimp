bf37e62e593a38d3bbac717dc6f2ca9a747aec8f
Dorian Turba <dturba.pro@protonmail.com>
2022-09-18 17:05:54 +0200

    [docs] correct 'basic' example code with set instead of bare string 'math'

--- examples/basic/package/__init__.py
+++ examples/basic/package/__init__.py
@@ -3,4 +3,4 @@ import lazimp
 math: lazimp.ModuleType
 heavy_module: lazimp.ModuleType
 
-__getattr__ = lazimp.lazy_import('math', {'heavy_module': 'package'})
+__getattr__ = lazimp.lazy_import({'math'}, {'heavy_module': 'package'})
