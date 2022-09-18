147cd27ad7422238b36a967bca1324eaef140b0a
Dorian Turba <dturba.pro@protonmail.com>
2022-09-18 16:54:49 +0200

    [feat] lazy load of alias import

--- src/lazimp/__init__.py
+++ src/lazimp/__init__.py
@@ -7,17 +7,22 @@ from types import ModuleType
 def lazy_import(
         bare_import: collections.abc.Container | None = None,
         sub_import: collections.abc.Mapping[str, str | None] | None = None,
+        aliases: collections.abc.Mapping[str, str] | None = None,
 ):
     if bare_import is None:
         bare_import = set()
     if sub_import is None:
         sub_import = {}
+    if aliases is None:
+        aliases = {}
 
     import functools
     import importlib
 
     @functools.cache
     def getattr_(name: str):
+        name = aliases.get(name, name)
+
         if name not in bare_import and name not in sub_import:
             raise AttributeError(
                 f'Module has no attribute {name!r}'
